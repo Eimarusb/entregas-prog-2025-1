e4fa95af20285880199a010453e148e430b514d3
Eimarusb <eegarciar@academia.usbbog.edu.co>
2025-03-13 21:21:26 -0500

    Update calculadora.py

--- Calculadora1/calculadora.py
+++ Calculadora1/calculadora.py
@@ -6,7 +6,7 @@ Calculadora1: Prototipo
 Descripción extendida del programa
 
 Autor: Eimar Esteban Garcia Ramirez <eegarciar@academia.usbbog.edu.co>/<eimarg522@gmail.com>
-Fecha: 2025-03-01
+Fecha: 2025-03-013
 """
 
 # **** En esta región puede importar los módulos necesarios para su programa
@@ -15,49 +15,30 @@ Fecha: 2025-03-01
 
 def run():
     """script entrypoint"""
-
-    # **** Poner el código ejecutable de su ejercicio aquí
-# Menú de selección de operación
-print("Escoge una operacion basica :):")
-print("1. Suma +")
-print("2. Resta -")
-print("3. Multiplicación *")
-print("4. División /")
-
-# Seleccción de operaciones
-selecciónDeUsuario = input("Ingrese el número de la operación que desea realizar (1-4): ")
-
-# Solicitar operandos solo si se va a realizar una operación
-primerTermino = int(input("Escriba un número entero: "))
-segundoTermino = int(input("Escriba otro número entero: "))
-
-# Ejecutar suma
-if selecciónDeUsuario  == '1':
-    resultado = primerTermino + segundoTermino
-    print(f"La suma de {primerTermino} y {segundoTermino} es {resultado}")
-# Ejecutar resta
-elif selecciónDeUsuario  == '2':
-    resultado = primerTermino - segundoTermino
-    print(f"La resta de {primerTermino} y {segundoTermino} es {resultado}")
-# Ejecutar multiplicacion
-elif selecciónDeUsuario == '3':
-    resultado = primerTermino * segundoTermino
-    print(f"La multiplicación de {primerTermino} y {segundoTermino} es {resultado}")
-# Ejecutar division
-elif selecciónDeUsuario == '4':
-    if segundoTermino != 0:  #signo ! significa distinto a...
-        resultado = primerTermino / segundoTermino
-        print(f"La división de {primerTermino} entre {segundoTermino} es {resultado}")
-    else:
-        print("Nunca dividas por cero 0")
-#si no se escoge opcion de 1-4, ejecutar de nuevo el codigo 
-else:
-    print("selecciona opciones 1/2/3/4.")
-
-
-    # Saludo
-    print("Hola mundo!")
-
+    print("Escoge una operacion basica :):")  
+    print("1. Suma +")
+    print("2. Resta -")
+    print("3. Multiplicación *")
+    print("4. División ")
+    seleccionDeUsuario = input("Ingrese el número de la operación que desea realizar (1-4): ")
+    primerTermino = int(input("Escriba un número entero: "))
+    segundoTermino = int(input("Escriba otro número entero: "))
+    if seleccionDeUsuario == '1':
+        resultado = primerTermino + segundoTermino
+        print(f"La suma de {primerTermino} y {segundoTermino} es {resultado}")
+    elif seleccionDeUsuario == '2':
+        resultado = primerTermino - segundoTermino
+        print(f"La resta de {primerTermino} y {segundoTermino} es {resultado}")
+    elif seleccionDeUsuario == '3':
+        resultado = primerTermino * segundoTermino
+        print(f"La multiplicación de {primerTermino} y {segundoTermino} es {resultado}")
+    elif seleccionDeUsuario == '4':
+        if segundoTermino != 0:
+            resultado = primerTermino / segundoTermino
+            print(f"La división de {primerTermino} entre {segundoTermino} es {resultado}")
+        else:
+            print("Nunca dividas por cero 0")
+ 
     # **** ****
 
 
